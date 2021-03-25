d613007428ce5c9167a5e1166226619a357c69a4
DungHoang <kathymaidung@gmail.com>
2021-03-25 08:58:21 +0200

    change basic skills part1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
       <h1 className="py-5">my skills</h1>
       <div className="container">
         <div className="row">
-          <div className="col-lg-3 col-md-6">
+          <div className="col-lg-6 col-md-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
@@ -22,43 +22,13 @@ export default function Skills() {
             </div>
           </div>
           {/* - */}
-          <div className="col-lg-3 col-md-6">
+          <div className="col-lg-6 col-md-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
               </div>
               <h3>Web Development</h3>
               <p>Your website will be build with a new proven technologies.</p>
-            </div>
-          </div>
-          {/* - */}
-          <div className="col-lg-3 col-md-6">
-            <div className="box">
-              <div className="circle">
-                <FontAwesomeIcon
-                  className="icon"
-                  icon={faFacebookF}
-                  size="2x"
-                />
-              </div>
-              <h3>Facebook Ads SMM</h3>
-              <p>
-                Your potential clients will see your services or product on
-                Facebook.
-              </p>
-            </div>
-          </div>
-          {/* - */}
-          <div className="col-lg-3 col-md-6">
-            <div className="box">
-              <div className="circle">
-                <FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
-              </div>
-              <h3>Google Ads</h3>
-              <p>
-                Your service or product will appear at the top of the Google
-                search.
-              </p>
             </div>
           </div>
         </div>
