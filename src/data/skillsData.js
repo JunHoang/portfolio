6e4f2f818ad128b245065ad29c8d15360efa0e94
DungHoang <kathymaidung@gmail.com>
2021-04-01 12:21:53 +0300

    Add skillsData and images

---
+++ src/data/skillsData.js
@@ -0,0 +1,197 @@
+import I_HTML from "../images/skills/html-icon.jpg";
+import I_CSS3 from "../images/skills/css-icon.png";
+import I_BOOTSTRAP from "../images/skills/bootstrap-icon.png";
+import I_JAVASCRIPT from "../images/skills/javascript-icon.jpg";
+import I_REACT from "../images/skills/reactjs-icon.png";
+import I_REACT_BOOTSTRAP from "../images/skills/react-bootstrap-icon.jpg";
+import I_REACT_ROUTER from "../images/skills/react-router.png";
+import I_REDUX from "../images/skills/redux-icon.jpg";
+import I_SASS from "../images/skills/sass-icon.png";
+import I_NODEJS from "../images/skills/nodejs-icon.png";
+import I_EXPRESS from "../images/skills/express-icon.png";
+import I_RESTAPI from "../images/skills/restapi-icon.png";
+import I_NETLIFY from "../images/skills/netlify-icon.png";
+import I_GITHUB_PAGES from "../images/skills/github-pages-icon.jpg";
+import I_HEROKU from "../images/skills/heroku-icon.png";
+import I_MONGODB from "../images/skills/mongodb-icon.png";
+import I_FIREBASE from "../images/skills/firebase-icon.jpg";
+import I_SQL from "../images/skills/sql-icon.png";
+import I_GIT from "../images/skills/git-icon.png";
+import I_NPM from "../images/skills/npm-icon.png";
+import I_YARN from "../images/skills/yarn-icon.jpg";
+
+export const skills = [
+  {
+    title: "Front-End",
+    tech: [
+      {
+        link: "https://en.wikipedia.org/wiki/HTML5",
+        imgAltText: "HTML5",
+        imgSrc: I_HTML,
+        name: "HTML5",
+      },
+      {
+        link: "https://www.w3schools.com/css/",
+        imgAltText: "CSS3",
+        imgSrc: I_CSS3,
+        name: "CSS",
+      },
+      {
+        link: "",
+        imgAltText: "Bootstrap",
+        imgSrc: I_BOOTSTRAP,
+        name: "Bootstrap",
+      },
+      {
+        link: "",
+        imgAltText: "Javascript",
+        imgSrc: I_JAVASCRIPT,
+        name: "Javascript",
+      },
+      {
+        link: "",
+        imgAltText: "React",
+        imgSrc: I_REACT,
+        name: "React JS",
+      },
+      {
+        link: "",
+        imgAltText: "React Bootstrap",
+        imgSrc: I_REACT_BOOTSTRAP,
+        name: "React Bootstrap",
+      },
+      {
+        link: "",
+        imgAltText: "React Router",
+        imgSrc: I_REACT_ROUTER,
+        name: "React Router",
+      },
+      {
+        link: "",
+        imgAltText: "Redux",
+        imgSrc: I_REDUX,
+        name: "Redux",
+      },
+      {
+        link: "",
+        imgAltText: "Sass",
+        imgSrc: I_SASS,
+        name: "Sass",
+      },
+    ],
+  },
+  {
+    title: "Back-End",
+    tech: [
+      {
+        link: "",
+        imgAltText: "Nodejs",
+        imgSrc: I_NODEJS,
+        name: "Node JS",
+      },
+      {
+        link: "",
+        imgAltText: "Express",
+        imgSrc: I_EXPRESS,
+        name: "Express",
+      },
+      {
+        link: "",
+        imgAltText: "RestAPI",
+        imgSrc: I_RESTAPI,
+        name: "RestAPI",
+      },
+    ],
+  },
+  {
+    title: "Hosting Platforms",
+    tech: [
+      {
+        link: "",
+        imgAltText: "Netlify",
+        imgSrc: I_NETLIFY,
+        name: "Netlify",
+      },
+      {
+        link: "",
+        imgAltText: "Heroku",
+        imgSrc: I_HEROKU,
+        name: "Heroku",
+      },
+      {
+        link: "",
+        imgAltText: "Github Pages",
+        imgSrc: I_GITHUB_PAGES,
+        name: "GitHub Pages",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    tech: [
+      {
+        link: "",
+        imgAltText: "MongoDB",
+        imgSrc: I_MONGODB,
+        name: "MongoDB",
+      },
+      {
+        link: "",
+        imgAltText: "SQL",
+        imgSrc: I_SQL,
+        name: "SQL",
+      },
+      {
+        link: "",
+        imgAltText: "Firebase",
+        imgSrc: I_FIREBASE,
+        name: "Firebase",
+      },
+    ],
+  },
+  {
+    title: "Version Control",
+    tech: [
+      {
+        link: "",
+        imgAltText: "Git",
+        imgSrc: I_GIT,
+        name: "Git",
+      },
+    ],
+  },
+  {
+    title: "Package Manager",
+    tech: [
+      {
+        link: "",
+        imgAltText: "Npm",
+        imgSrc: I_NPM,
+        name: "NPM",
+      },
+      {
+        link: "",
+        imgAltText: "Yarn",
+        imgSrc: I_YARN,
+        name: "Yarn",
+      },
+    ],
+  },
+  {
+    title: "Other Knowledge",
+    tech: [
+      {
+        name: "Networking - CCNA",
+      },
+      {
+        name: "Networking Security",
+      },
+      {
+        name: "Beginer of AWS",
+      },
+      {
+        name: "Basic of IoT",
+      },
+    ],
+  },
+];
